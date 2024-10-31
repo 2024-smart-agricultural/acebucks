17dec839c4c700c82e2fbbd8ee1c9cfef82d4c32
in-ae <101018070+in-ae@users.noreply.github.com>
2024-10-31 16:32:31 +0900

    Update regional_item_prices.py

--- .github/regional_item_prices.py
+++ .github/regional_item_prices.py
@@ -4,6 +4,13 @@ import requests
 from datetime import datetime
 import subprocess
 
+# 원하는 키워드 리스트
+desired_keywords = [
+    '고구마', '수박', '토마토', '딸기', '당근', '멜론', '사과',
+    '배', '복숭아', '포도', '감귤', '단감', '바나나', '파인애플',
+    '오렌지', '자몽', '레몬', '체리', '망고', '블루베리'
+]
+
 def get_regional_item_prices():
     try:
         api_key = os.getenv('KAMIS_KEY')
@@ -14,8 +21,10 @@ def get_regional_item_prices():
 
         if response.status_code == 200:
             data = response.json()
+            # 특정 키워드가 포함된 데이터만 필터링
+            filtered_data = filter_desired_items(data)
             item_info = {
-                "all_data": data,  # 전체 데이터 저장
+                "all_data": filtered_data,  # 필터링된 데이터 저장
                 "datetime": datetime.now().strftime("%Y-%m-%d %H:%M:%S")
             }
             return item_info
@@ -26,9 +35,17 @@ def get_regional_item_prices():
         print(f"Error occurred while fetching regional item prices: {e}")
         return None
 
+def filter_desired_items(data):
+    filtered_items = []
+    for item in data.get('data', {}).get('item', []):
+        product_name = item.get('productName', '')
+        if any(keyword in product_name for keyword in desired_keywords):
+            filtered_items.append(item)
+    return {"item": filtered_items}  # 필터링된 품목 리스트 반환
+
 def save_regional_item_prices():
     new_data = get_regional_item_prices()
-    if new_data:
+    if new_data and new_data['all_data']['item']:
         with open('docs/regional_item_prices.json', 'w', encoding='utf-8') as file:
             json.dump(new_data, file, ensure_ascii=False, indent=4)
         print("regional_item_prices.json created and data saved.")
