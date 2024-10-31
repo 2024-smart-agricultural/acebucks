05163e4dcdd774d93742a829ecc955bf6c2259b4
in-ae <101018070+in-ae@users.noreply.github.com>
2024-10-31 10:29:18 +0900

    Update daily_prices_by_category.py

--- .github/daily_prices_by_category.py
+++ .github/daily_prices_by_category.py
@@ -1,6 +1,8 @@
 import os
 import json
 import requests
+from datetime import datetime
+import subprocess
 
 def get_daily_prices_by_category():
     try:
@@ -12,11 +14,43 @@ def get_daily_prices_by_category():
 
         if response.status_code == 200:
             data = response.json()
-            with open('docs/daily_prices_by_category.json', 'w', encoding='utf-8') as f:
-                json.dump(data, f, ensure_ascii=False, indent=4)
-
+            category_info = {
+                "all_data": data,  # 전체 데이터 저장
+                "datetime": datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+            }
+            return category_info
+        else:
+            print(f"Failed to fetch data for daily prices by category. Status code: {response.status_code}")
+            return None
     except Exception as e:
-        print(f"Error fetching daily prices by category: {e}")
+        print(f"Error occurred while fetching daily prices by category: {e}")
+        return None
+
+def save_daily_prices_by_category():
+    new_data = get_daily_prices_by_category()
+    if new_data:
+        with open('docs/daily_prices_by_category.json', 'w', encoding='utf-8') as file:
+            json.dump(new_data, file, ensure_ascii=False, indent=4)
+        print("daily_prices_by_category.json created and data saved.")
+    else:
+        print("No daily prices by category data collected.")
+
+    commit_and_push_changes()
+
+def commit_and_push_changes():
+    subprocess.run(["git", "config", "--global", "user.email", "you@example.com"])
+    subprocess.run(["git", "config", "--global", "user.name", "Your Name"])
+
+    # JSON 파일을 스테이징
+    subprocess.run(["git", "add", "docs/daily_prices_by_category.json"])
+
+    # 변경 사항이 있을 때만 커밋
+    result = subprocess.run(["git", "diff", "--cached", "--quiet"])
+    if result.returncode != 0:
+        subprocess.run(["git", "commit", "-m", "Update daily prices by category data"])
+        subprocess.run(["git", "push"])
+    else:
+        print("No changes to commit.")
 
 if __name__ == "__main__":
-    get_daily_prices_by_category()
+    save_daily_prices_by_category()
