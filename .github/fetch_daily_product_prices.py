3549ef8129f3e7bc5412f90d4504badc8729d38c
in-ae <101018070+in-ae@users.noreply.github.com>
2024-11-01 14:29:20 +0900

    Update fetch_daily_product_prices.py

--- .github/fetch_daily_product_prices.py
+++ .github/fetch_daily_product_prices.py
@@ -21,17 +21,31 @@ def fetch_daily_prices():
     response = requests.get(URL, params=params)
     if response.status_code == 200:
         data = response.json()
+
+        # NaN 값을 null로 변환
+        def replace_nan(obj):
+            if isinstance(obj, list):
+                return [replace_nan(i) for i in obj]
+            elif isinstance(obj, dict):
+                return {k: replace_nan(v) for k, v in obj.items()}
+            elif isinstance(obj, float) and np.isnan(obj):
+                return None
+            else:
+                return obj
+
+        cleaned_data = replace_nan(data)
     
         # 기존 JSON 파일 불러오기 또는 새로운 파일 생성
         json_file_path = 'docs/daily_rpoduct_prices.json'
         if os.path.exists(json_file_path):
             with open(json_file_path, 'r', encoding='utf-8') as f:
                 existing_data = json.load(f)
-            # 기존 데이터에 새로운 데이터 추가
-            existing_data.append(data)
+            # 기존 데이터에 새로운 데이터가 없을 때만 추가
+            if cleaned_data not in existing_data:
+                existing_data.append(cleaned_data)
         else:
-            existing_data = [data]
-
+            existing_data = [cleaned_data]
+    
         # 업데이트된 데이터를 JSON 파일로 저장
         with open(json_file_path, 'w', encoding='utf-8') as f:
             json.dump(existing_data, f, ensure_ascii=False, indent=4)
